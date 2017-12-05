dc45d3528aa4e8dddd9d2767d0ee2e443bd5780b
zhangxun <zhangxun@apuecloud.com>
2017-12-05 15:50:21 +0800

    完善 alias

--- apps/alias/serializers.py
+++ apps/alias/serializers.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Author:zhangxun
+
+from rest_framework import serializers
 from PublicFunc.serializers import AllSerializer
 from .models import Alias
 
@@ -8,6 +10,21 @@ class AliasSerializer(AllSerializer):
     """
     序列化 Models.Alias
     """
+    def validate(self, data):
+        """
+        检验 old_ip 与 start_ip、end_ip，判断三者是否同时存在
+        """
+        if data['ip_choice'] == 0:
+            # 用户选择 单ip, 则置空 ip段
+            data['start_ip'] = ""
+            data['end_ip'] = ""
+        elif data['ip_choice'] == 1:
+            # 选择 ip段, 则置空 原ip
+            data['old_ip'] = ""
+        else:
+            raise serializers.ValidationError("「原IP」与「起始IP」、「结束IP」不能同时存在")
+        return data
+
     class Meta:
         model = Alias
         fields = '__all__'
--- apps/alias/views.py
+++ apps/alias/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 from IPy import IP
@@ -13,48 +12,68 @@ class AliasViewset(viewsets.ModelViewSet):
     """
     允许用户查看或编辑 Alias API
     """
-    # 存放在 POST 方法
-    # if Alias.ip_choice == 0:
-    #     queryset = Alias.objects.defer('start_ip', 'end_ip')
-    # else:
-    #     queryset = Alias.objects.defer('old_ip')
     queryset = Alias.objects.all()
     serializer_class = AliasSerializer
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.validated_data['create_user'] = self.request.user.username
-            serializer.validated_data['update_user'] = self.request.user.username
-            # serializer.validated_data['old_ip'] = IP(serializer.validated_data['old_ip']).strBin()
-            # serializer.validated_data['start_ip'] = IP(serializer.validated_data['start_ip']).strBin()
-            # serializer.validated_data['end_ip'] = IP(serializer.validated_data['end_ip']).strBin()
-            # serializer.validated_data['new_ip'] = IP(serializer.validated_data['new_ip']).strBin()
-            self.perform_create(serializer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['create_user'] = self.request.user.username
+        serializer.validated_data['update_user'] = self.request.user.username
+        if serializer.validated_data['ip_choice'] == 0:
+            serializer.validated_data['old_ip'] = IP(serializer.validated_data['old_ip']).strBin()
+            serializer.validated_data['start_ip'] = ""
+            serializer.validated_data['end_ip'] = ""
+        elif serializer.validated_data['ip_choice'] == 1:
+            serializer.validated_data['old_ip'] = ""
+            serializer.validated_data['start_ip'] = IP(serializer.validated_data['start_ip']).strBin()
+            serializer.validated_data['end_ip'] = IP(serializer.validated_data['end_ip']).strBin()
+        serializer.validated_data['new_ip'] = IP(serializer.validated_data['new_ip']).strBin()
+        self.perform_create(serializer)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+            根据Id获取域名解析相关信息，并将二进制IP转换为点分十进制
+        """
+        instance = self.get_object()
+        if instance.old_ip:
+            instance.old_ip = ip_bin2int(instance.old_ip)
+        if instance.start_ip:
+            instance.start_ip = ip_bin2int(instance.start_ip)
+        if instance.end_ip:
+            instance.end_ip = ip_bin2int(instance.end_ip)
+        instance.new_ip = ip_bin2int(instance.new_ip)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial )
-        if serializer.is_valid():
-            serializer.validated_data['create_user'] = self.request.user.username
-            serializer.validated_data['update_user'] = self.request.user.username
-            # serializer.validated_data['old_ip'] = IP(serializer.validated_data['old_ip']).strBin()
-            # serializer.validated_data['start_ip'] = IP(serializer.validated_data['start_ip']).strBin()
-            # serializer.validated_data['end_ip'] = IP(serializer.validated_data['end_ip']).strBin()
-            # serializer.validated_data['new_ip'] = IP(serializer.validated_data['new_ip']).strBin()
-            self.perform_update(serializer)
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['update_user'] = self.request.user.username
+        if serializer.validated_data['ip_choice'] == 0:
+            serializer.validated_data['old_ip'] = IP(serializer.validated_data['old_ip']).strBin()
+        elif serializer.validated_data['ip_choice'] == 1:
+            serializer.validated_data['start_ip'] = IP(serializer.validated_data['start_ip']).strBin()
+            serializer.validated_data['end_ip'] = IP(serializer.validated_data['end_ip']).strBin()
+        serializer.validated_data['new_ip'] = IP(serializer.validated_data['new_ip']).strBin()
+        self.perform_update(serializer)
+        return Response(serializer.data)
 
     def get_queryset(self):
         queryset = Alias.objects.all()
-        agt_id = self.request.query_params('agt_id', None)
+        agt_id = self.request.query_params.get('agt_id', None)
         if agt_id:
             queryset = queryset.filter(agt_id=agt_id)
         for i in queryset:
-            i.agt_ip = ip_bin2int(i.agt_ip)
+            if i.old_ip:
+                i.old_ip = ip_bin2int(i.old_ip)
+            if i.start_ip:
+                i.start_ip = ip_bin2int(i.start_ip)
+            if i.end_ip:
+                i.end_ip = ip_bin2int(i.end_ip)
+            i.new_ip = ip_bin2int(i.new_ip)
         return queryset
 
